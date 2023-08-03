7d238ff38a7332596236e802cc281d7a10de1817
npfabp10 <npfabp10@gmail.com>
2023-08-02 21:03:49 -0300

    Initial Commit

---
+++ App0-Demo/App0_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  App0_DemoApp.swift
+//  App0-Demo
+//
+//  Created by Nfabp10 on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct App0_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
